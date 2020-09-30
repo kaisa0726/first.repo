4e0b395d00bd9041fad20f4c0a7f12f111f5546b
hogehoge <test@ec.jp>
2020-09-30 16:52:15 +0900

    lesson03

--- mamazon/models.py
+++ mamazon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length = 150,verbose_name = "入力して下さい")
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to = 'thumbnails/')
+
