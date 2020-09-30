6365a4482c70e6f75586dc99aa9178e0d38401a0
hogehoge <test@ec.jp>
2020-09-30 20:45:03 +0900

    losson04

--- mamazon/models.py
+++ mamazon/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length = 150,verbose_name = "入力して下さい")
+    name = models.CharField(max_length = 150)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to = 'thumbnails/')
+
+    def __str__(self):
+        return self.name
+
 
